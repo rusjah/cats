9870819372dce6a7a5fe8cd6cc543e1b260ed8b5
ruslana_hural <rusjah1@gmail.com>
2023-11-21 14:46:19 +0100

    made home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <figcaption className='mainDescr'>
+            <p>A happy arrangement: many people prefer cats to other people, and many cats prefer people to other cats.</p>
+            <Link to={'/cats'}> 
+            {/* <button>Let's go</button> */}
+            <input type="image" src="https://webstockreview.net/images/clipart-dog-paw-print-6.jpg" alt=""/>
+            <p className='goBtn'>Let's go</p>
+            </Link>
+        </figcaption>
+        <div >
+            <figure className='mainPhoto'>
+                 <img src="https://images.unsplash.com/photo-1634117766879-dc33d27501c5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHBhdyUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D" alt="cat" />
+             </figure>
+        </div>
+        
+    </div>
   )
 }
 
