d1de042fd053b053bccee6627f40abe044be78e0
ruslana_hural <rusjah1@gmail.com>
2023-11-21 20:08:29 +0100

    added style for media

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
         </figcaption>
         <div >
             <figure className='mainPhoto'>
+                 {/* <img src="https://images.unsplash.com/photo-1450778869180-41d0601e046e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGRvZyUyMGFuZCUyMGNhdCUyMGZyaWVuZHN8ZW58MHx8MHx8fDA%3D" alt="cat" /> */}
                  <img src="https://images.unsplash.com/photo-1634117766879-dc33d27501c5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHBhdyUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D" alt="cat" />
              </figure>
         </div>
