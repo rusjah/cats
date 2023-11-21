37d20571e805a9f605c9c7a99fdeb5d742446efa
ruslana_hural <rusjah1@gmail.com>
2023-11-21 14:57:31 +0100

    rendered list of cats and add listenter to Look-btn

--- src/components/pages/Cats.jsx
+++ src/components/pages/Cats.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import cats from '../../data/cats.json'
+import { useNavigate } from 'react-router-dom'
+
+
 function Cats() {
+    const navigate = useNavigate();
+    
+    function clickHandler () {
+        navigate('/cat')
+    }
   return (
-    <div>Cats</div>
+    <div className='cats'>
+        {cats.map((ct, ind) => <div key={ind}>
+            <h2>{ct.name}</h2>
+            <button onClick={clickHandler}>Look</button>
+        </div>)}
+    </div>
   )
 }
 
